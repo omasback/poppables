0c6a8e4977a877c25a0e4ceee8b38c44f9bc682d
Owen Masback <omasback@gmail.com>
2017-01-05 20:20:47 -0800

    add pop interaction to redbubble

--- webpack/components/redBubble/index.vue
+++ webpack/components/redBubble/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="redBubble">
+  <div class="redBubble" v-on:click="onClick">
   </div>
 </template>
 
@@ -20,8 +20,26 @@ export default {
       },
     }
   },
+  mounted: function() {
+    this.bodyMover.onEnterFrame = goTo30;
+  },
+  methods: {
+    onClick: function() {
+      this.bodyMover.onEnterFrame = null;
+      this.bodyMover.goToAndPlay(151, true);
+      this.bodyMover.onLoopComplete = () => {
+        this.bodyMover.goToAndPlay(0, true);
+        this.bodyMover.onEnterFrame = goTo30;
+      };
+    }
+  }
 }
 
+function goTo30(){
+  if (this.currentFrame > 150) {
+    this.goToAndPlay(30, true);
+  }
+}
 </script>
 
 <style lang="sass" scoped>
--- webpack/util/bodyMoverMixin.js
+++ webpack/util/bodyMoverMixin.js
@@ -2,7 +2,6 @@ import bodymovin from 'bodymovin';
 
 export default {
   created: function () {
-    console.log(this);
     this.bmOptions.animationData.assets.forEach(asset => {
       if (!asset.layers) {
         asset.p = this.contextModule(`./${asset.p}`);
