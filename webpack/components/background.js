84f555be3e5f2cb6e24017d2fb50ec92a5526483
Owen Masback <omasback@gmail.com>
2017-01-09 18:30:51 -0800

    background dots

--- webpack/components/background.js
+++ webpack/components/background.js
@@ -1,6 +1,7 @@
 import glvbg from 'gl-vignette-background'
 import hex from 'hex-rgb'
-import debounce from 'lodash/debounce';
+import debounce from 'lodash/debounce'
+import webglContext from 'webgl-context'
 
 function rgb (str) {
   return hex(str).map(function (a) {
@@ -9,16 +10,16 @@ function rgb (str) {
 }
 
 export default function bg() {
-  const canvas = document.querySelector('.background')
+  const vignetteCanvas = document.querySelector('.vignette')
 
   function drawBg() {
 
     const startTime = performance.now();
-    const w = canvas.clientWidth
-    const h = canvas.clientHeight
+    const w = vignetteCanvas.clientWidth
+    const h = vignetteCanvas.clientHeight
 
-    const gl = require('webgl-context')({
-      canvas: canvas, //the canvas DOM element to use
+    const gl = webglContext({
+      canvas: vignetteCanvas, //the canvas DOM element to use
       width: w, //resizes the canvas..
       height: h,
       // antialias: true //can specify custom attributes here
@@ -43,6 +44,33 @@ export default function bg() {
       offset: [ 0, -0.15 ]
     })
     bg.draw()
+
+    const dotsCanvas = document.querySelector('.dots')
+    dotsCanvas.width = w
+    dotsCanvas.height = h
+    const dotsCtx = dotsCanvas.getContext('2d')
+    const startingRadius = 22
+    const spacing = 78
+    const numRows = 14
+
+    const cols = w / spacing + 1;
+    for (let row = 0; row < numRows; row++) {
+      let opacity = (numRows - row) / numRows;
+      dotsCtx.fillStyle = `rgba(0, 173, 183, ${opacity})`
+      let y = h - row * spacing / 2
+      let radius = startingRadius - (row * startingRadius / numRows)
+
+      for (let col = 0; col < cols; col++) {
+        let x = col * spacing
+        if (row % 2) {
+          x += spacing / 2
+        }
+        dotsCtx.beginPath()
+        dotsCtx.arc(x, y, radius, 0, Math.PI * 2, true)
+        dotsCtx.fill()
+        dotsCtx.closePath()
+      }
+    }
 
     console.log(`drawBg: ${(performance.now() - startTime).toFixed(1)}ms`)
   }
