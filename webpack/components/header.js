bcf5c966f89508fa28b6135bf16b97d5b99a0216
kalisafalzone <kalisa.falzone@gmail.com>
2017-01-10 18:36:08 -0800

    adds x and hamburger icons to mobile view for nav bar, switches between the icons on click

--- webpack/components/header.js
+++ webpack/components/header.js
@@ -7,5 +7,13 @@ export default function header() {
   headerToggle.addEventListener('click', () => {
     navShowing = !navShowing;
     //do DOM stuff here to show or hide the nav
+    console.log('navShowing is', navShowing)
+    if (navShowing) {
+      headerNav.style.display = 'block';
+      headerToggle.children[0].className = "navImage xIcon";
+    } else {
+      headerNav.style.display = 'none';
+      headerToggle.children[0].className = "navImage hamburger";
+    }
   })
 }
