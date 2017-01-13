51a0d0b7bd975330d6b640f7536180185abb003a
kalisafalzone <kalisa.falzone@gmail.com>
2017-01-12 17:39:18 -0800

    adds transisition to blurred elements and re-factors to use classList

--- webpack/components/header.js
+++ webpack/components/header.js
@@ -10,23 +10,25 @@ export default function header() {
     navShowing = !navShowing;
     if (navShowing) {
       headerNav.style.display = 'block';
-      headerToggle.children[0].className = "navImage xIcon";
-      content.className = "contentContainer blurred";
+      headerToggle.children[0].classList.add("xIcon");
+      headerToggle.children[0].classList.remove("hamburger");
+      content.classList.add('blurred');
     } else {
       headerNav.style.display = 'none';
-      headerToggle.children[0].className = "navImage hamburger";
-      content.className = "contentContainer";
+      headerToggle.children[0].classList.remove("xIcon");
+      headerToggle.children[0].classList.add("hamburger");
+      content.classList.remove('blurred');
     }
   })
   //function to re-size the logo
   window.addEventListener('scroll', () => {
     let yScroll = window.pageYOffset;
     if (yScroll < 1) { //large logo
-      headerLogoImage.className = "headerLogoImage"
-      headerBar.className = 'headerBar';
+      headerLogoImage.classList.remove('headerLogoImageSmall');
+      headerBar.classList.remove('headerBarScroll');
     } else { //small logo
-      headerLogoImage.className = "headerLogoImage headerLogoImageSmall"
-      headerBar.className = 'headerBar headerBarScroll';
+      headerLogoImage.classList.add('headerLogoImageSmall');
+      headerBar.classList.add('headerBarScroll');
     } 
   })
 }
