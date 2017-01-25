ce0a252c42ec7108e639f4a4af3b2d006aae26ed
Owen Masback <omasback@gmail.com>
2017-01-24 19:03:45 -0800

    only run bg on homepage

--- webpack/application.js
+++ webpack/application.js
@@ -3,12 +3,10 @@
 import './styles/application.scss'
 import $ from 'jquery'
 
-import bg from './components/background.js'
 import header from './components/header.js'
 
-bg();
 header();
 
 $(() => {
-    
+
 })
--- webpack/home.js
+++ webpack/home.js
@@ -3,9 +3,10 @@ import Vue from 'vue'
 import './application'
 import Home from './components/home.vue'
 
-if (document.getElementById('home')) {
-  const vm = new Vue({
-    el: '#home',
-    ...Home,
-  })
-}
+import bg from './components/background.js'
+
+bg();
+const vm = new Vue({
+  el: '#home',
+  ...Home,
+})
