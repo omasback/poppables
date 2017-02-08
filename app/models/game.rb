d4eec61a896c4b6e97e76f404a1c162c3bd5ebe2
Melanie Windh <melanie.windh@gmail.com>
2017-02-08 14:35:17 -0800

    Update Game landing copy; POP-90

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,15 +1,15 @@
 module Game
   NAMES = {
     pops: {
-      title: 'Pops',
+      title: 'Pop the Poppables',
+      how_to_play: 'Tap or click the Poppables as fast as you can. The more you pop in a row, the bigger your Flavor Bonus!',
+      pro_tip: 'Clicking empty bubbles will decrease your power!',
       min_win_time: 15,
     },
     dots: {
-      title: 'Dots',
-      min_win_time: 15,
-    },
-    catch: {
-      title: 'Catch',
+      title: 'Pop & Drop',
+      how_to_play: 'Connect similar icons to remove them from the board. Connect Poppables for a Flavor Bonus!',
+      pro_tip: 'Make longer chains to score more points!',
       min_win_time: 15,
     },
   }.freeze
