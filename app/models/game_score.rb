e61d051e8daab99ea7a1f1fdeb7712d03383d4ed
David Hwang <david.hwang@hyfn.com>
2017-02-14 12:30:54 -0800

    POS-118 registration flow refactor and test: add copy (add changes)

--- app/models/game_score.rb
+++ app/models/game_score.rb
@@ -1,17 +1,11 @@
 require 'set'
 
 class GameScore < ActiveRecord::Base
-  DIRTY_WORDS = %w(666 $3X $EX A$$ A$5 A$S A$S A5$ A55 AS$ ASS BCH BUM BUT CNT COK C0K CUM D!3 D!E D1E D1E DCK DIE DIK F@G F@T F4G F4T FAG FAT FUC FUK FUX G@Y G0D G4Y GAY GOD GUN IRA J!Z J1Z JIZ KKK KNT KOK LIK LSD NIG P00 P33 P5Y PEE POO POT PSY S3X SEX SHT SUK T!T TIT VAG VAJ)
-
-  @dirty_words_hash = Set.new(DIRTY_WORDS)
-  class << self
-    attr_reader :dirty_words_hash
-  end
+  DIRTY_WORDS = Set.new(%w(666 $3X $EX A$$ A$5 A$S A$S A5$ A55 AS$ ASS BCH BUM BUT CNT COK C0K CUM D!3 D!E D1E D1E DCK DIE DIK F@G F@T F4G F4T FAG FAT FUC FUK FUX G@Y G0D G4Y GAY GOD GUN IRA J!Z J1Z JIZ KKK KNT KOK LIK LSD NIG P00 P33 P5Y PEE POO POT PSY S3X SEX SHT SUK T!T TIT VAG VAJ).freeze)
 
   validates :initials, :score, :game, presence: true
   validates :game, inclusion: { in: Game::NAMES.keys.map(&:to_s) }
-  validates :initials, length: { is: 3 }
-  validate  :initials_are_not_dirty
+  validates :initials, length: { is: 3 }, exclusion: { in: DIRTY_WORDS, message: 'are not allowed' }
 
   def self.for_game(game)
     where(game: game)
@@ -19,13 +13,5 @@ class GameScore < ActiveRecord::Base
 
   def rank
     GameScore.for_game(game).order(score: :desc).where('score > ?', score).count + 1
-  end
-
-  protected
-
-  def initials_are_not_dirty
-    if initials && self.class.dirty_words_hash.include?(initials.upcase)
-      errors.add(:base, "Sorry, please choose different initials.")
-    end
   end
 end
