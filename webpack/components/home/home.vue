baadf2dba51a3de7d71538c8a453ae48d33f8ee2
Owen Masback <omasback@gmail.com>
2017-02-15 12:16:02 -0800

    POP-126 Homepage > Move up headline copy

--- webpack/components/home/home.vue
+++ webpack/components/home/home.vue
@@ -239,7 +239,7 @@ export default {
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -150%);
   text-align: center;
   color: #1ac5cd;
   z-index: 0;
