6357eb7de5b111bc4b088e23e516d0b23372f51b
Daniel Leavitt <daniel.leavitt@gmail.com>
2017-02-15 20:20:45 -0800

    CI updates

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,9 @@ ENV['COUPON_OFFER_CODE'] = '12345'
 ENV['COUPON_CHECK_CODE'] = '123455'
 ENV['COUPON_SHORT_CIPHER_KEY'] = 'abcdefgh'
 ENV['COUPON_LONG_CIPHER_KEY'] = 'abcdefghijklmnopqrstuvwxyz'
+ENV['SECRET_TOKEN'] ||= 'b0dc1287a3007377bbf3c823d5c416a94de5c0c6b368c3cf37a001ae39fb3c347ea1ad7cf359262beadc85260a734cfcd1dbac779df81ef4a1cbaba2dfe5e0b0'
+ENV['DEVISE_SECRET'] ||= '41a56c248569d65afe82d0048e567b35a9629f400f012aa1a9e2cffdc568c78c2e0cd54bb2cef8924335a2b36618db4a08673dba71534f9410a48fee41227e94'
+ENV['TEST_DATABASE_URL'] ||= 'postgres://localhost/poppables_test'
 
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
