16de5b6061102b0f8010600687f1cbc5f276a604
Melanie Windh <melanie.windh@gmail.com>
2017-02-16 15:14:47 -0800

    Update form error states; POP-59 POP-69

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,16 @@ module Poppables
 
     Rails.application.routes.default_url_options[:host] = ENV.fetch('APPLICATION_HOST')
 
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+      class_attr_index = html_tag.index 'class="'
+
+      if class_attr_index
+        html_tag.insert class_attr_index+7, 'error '
+      else
+        html_tag.insert html_tag.index('>'), ' class="error"'
+      end
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
