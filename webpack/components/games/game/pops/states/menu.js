7eab93b6c36929f931303436c655719f5e95aaf5
Troy Curnel <NearSingularity@troycurnel.com>
2017-02-17 12:02:50 -0800

    Sound Plays Sooner

--- webpack/components/games/game/pops/states/menu.js
+++ webpack/components/games/game/pops/states/menu.js
@@ -5,6 +5,10 @@ export default class extends Phaser.State {
   }
 
   create() {
-    
+    this.bg = this.game.add.audio('background', .1);
+    this.bg.loop = true;
+    this.game.sound.setDecodedCallback([ this.bg ], () => {
+      this.bg.play();
+    }, this);
   }
 }
