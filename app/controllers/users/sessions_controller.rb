ddcf82a192335f521905e9802b95e8acea659e83
Tom Krenzke <tom.krenzke@hyfn.com>
2017-02-20 10:29:29 -0800

    POP-59 simplify registration page, no longer use game score or token or anything like that

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,9 @@
 class Users::SessionsController < Devise::SessionsController
-  include GameResultFlash
-  before_action :verify_game_params!, only: [:new]
-  before_action :create_default_flash, only: [:new]
-  before_action :verify_game_flash!, only: [:create]
-  before_action :keep_flash, only: [:new, :create]
+  # include GameResultFlash
+  # before_action :verify_game_params!, only: [:new]
+  # before_action :create_default_flash, only: [:new]
+  # before_action :verify_game_flash!, only: [:create]
+  # before_action :keep_flash, only: [:new, :create]
 
   # GET /resource/sign_in
   def new
@@ -11,13 +11,13 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   # POST /resource/sign_in
-  def create
-    if user = User.find_by(email: params[:user][:email])
-      perform_redemption_and_then_render_or_redirect(user)
-    else
-      throw(:warden, scope: :user, message: :not_found_in_database)
-    end
-  end
+  # def create
+  #   if user = User.find_by(email: params[:user][:email])
+  #     perform_redemption_and_then_render_or_redirect(user)
+  #   else
+  #     throw(:warden, scope: :user, message: :not_found_in_database)
+  #   end
+  # end
 
   # DELETE /resource/sign_out
   # def destroy
