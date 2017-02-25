b40d83159a86ff32c036467a1dc93f4f54db3301
Troy Curnel <NearSingularity@troycurnel.com>
2017-02-24 18:21:51 -0800

    Game Screen Update

--- webpack/components/games/game/pops/Pops.vue
+++ webpack/components/games/game/pops/Pops.vue
@@ -89,11 +89,11 @@
     <div class="screen" slot="instruction-content">
       <p class="small-title">How to play:</p>
       <p class="prompt">Tap or click the Poppables as fast as you can. The more you pop in a row, the bigger your Flavor Bonus!</p>
-      <p class="pro-tip">Pro Tip: Clicking empty bubbles will decrease your power!</p>
       <video width="100%" controls autoplay loop class="preview">
         <source src="https://dcyb5ui1o0ebh.cloudfront.net/static/videos/preview-pops.mp4" type="video/mp4">
         <source src="https://dcyb5ui1o0ebh.cloudfront.net/static/videos/preview-pops.webm" type="video/webm">
       </video>
+      <p class="pro-tip">Pro Tip: Clicking empty bubbles will decrease your power!</p>
       <button @click="playGame(3)">START PLAYING</button>
     </div>
 
