9cf52a1882030baf671acf00a5aa86879bdbc292
Troy Curnel <NearSingularity@troycurnel.com>
2017-03-17 16:51:23 -0700

    Perfect Resizing for real

--- webpack/components/games/game/catch/objects/Grass.js
+++ webpack/components/games/game/catch/objects/Grass.js
@@ -18,9 +18,9 @@ export default class extends Phaser.Sprite {
   }
 
   resize() {
-    let scalarX = this.game.width < 768 ? (this.game.width / 768) : 1;
-    let scalarY = this.game.height / 2 < (125 * 3) ? ((this.game.height / 2) / 3) / 125 : 1;
     let offsetY = this.game.device.mobileSafari && !this.game.device.iPad ? 44 : this.game.device.iPad ? 42 : 0;
+    let scalarX = this.game.width < 768 ? (this.game.width / 768) : 1;
+    let scalarY = this.game.height / 2 < (125 * 3) ? ((this.game.height / 2) / 3) / (125 + offsetY) : 1;
 
     this.scale.setTo(scalarX, scalarY);
     this.x = 0;
