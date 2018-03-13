05890612f488af45a85bbd17ca415abf017c376b
Owen Masback <omasback@gmail.com>
2018-03-12 17:08:06 -0700

    front end form validation

--- webpack/pages.js
+++ webpack/pages.js
@@ -1,8 +1,10 @@
 import './components/application'
 import bg from './components/background.js'
+import form from './components/form.js'
 
 const loadPages = () => {
   bg();
+  form();
 
   window.removeEventListener('load', loadPages);
 }
